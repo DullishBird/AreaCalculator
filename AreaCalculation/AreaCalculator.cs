1249084aeeed8bd985f62af568462c8b0d44cb63
DullishBird <dullishbird@icloud.com>
2022-05-05 23:52:01 +0500

    Add project files.

---
+++ AreaCalculation/AreaCalculator.cs
@@ -0,0 +1,49 @@
+﻿namespace AreaCalculation
+{
+    public interface IAreaCalculation
+    {
+        public double GetArea(params double[] list);
+    }
+
+    public class TriangleArea : IAreaCalculation
+    {
+        double IAreaCalculation.GetArea(params double[] args)
+        {
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Invalid arguments count.");
+                return 0.0;
+            }
+
+            if (args[0] < 0 || args[1] < 0 || args[2] < 0)
+            {
+                Console.WriteLine("Triangle's side length cannot be negative"); //TODO : Must be replaced to Exceptions
+                return 0;
+            }
+
+            double p = (args[0] + args[1] + args[2]) / 2;
+
+            return (double)Math.Sqrt(p * (p - args[0]) * (p - args[1]) * (p - args[2]));
+        }
+    }
+
+    public class CircleArea : IAreaCalculation
+    {
+        double IAreaCalculation.GetArea(params double[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Invalid arguments count.");
+                return 0.0;
+            }
+
+            if (args[0] < 0)
+            {
+                Console.WriteLine("Radius cannot be negative."); //TODO : Must be replaced to Exceptions
+                return 0;
+            }
+
+            return (double)(Math.PI * (Math.Pow(args[0], 2)));
+        }
+    }
+}
---
+++ Sandbox/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AreaCalculation;
+
+public class RectangleArea : IAreaCalculation
+{
+    double IAreaCalculation.GetArea(params double[] args)
+    {
+        if (args.Length < 2)
+        {
+            Console.WriteLine("Invalid arguments count for rectangle area calculation.");
+            return 0.0;
+        }
+        if (args[0] < 0 || args[1] < 0)
+        {
+            Console.WriteLine("Rectangl's side length cannot be negative"); //TODO : Must be replaced to Exceptions
+            return 0;
+        }
+        return args[0] * args[1];
+    }
+}
+
+// 1. Вычисление площади фигуры без знания типа фигуры в compile-time
+// 2. Юнит-тесты
+// 3. Проверку на то, является ли треугольник прямоугольным
+
+
+
+
+
